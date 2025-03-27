c69fdbba68e7ba9d001d6065c0e6675a076379ac
thxwat <tanawatmiracle@gmail.com>
2025-03-27 18:31:05 +0700

    fixed addcg button

--- src/app/(campgroundinfo)/campground/page.tsx
+++ src/app/(campgroundinfo)/campground/page.tsx
@@ -14,26 +14,29 @@ export default async function Campground() {
   if (!campgrounds) return null;
 
   return (
-    <main className="text-center p-5 mx-[8%]">
+    <main className="flex flex-col text-center p-5 mx-[8%]">
       <div className="text-4xl font-bold m-10 mt-20 text-left">Campground</div>
+      <div>
+        <Suspense
+          fallback={
+            <div className="flex justify-center items-center h-40">
+              <CircularProgress color="primary" />
+            </div>
+          }
+        >
+          <CampgroundCatalog campgroundJson={campgrounds} />
+        </Suspense>
+      </div>
+      <div className="bottom-10 flex justify-center w-full">
+        {profile.data.role == "admin" ? (
+          <Link href="/campground/manage/add">
+            <button className="bg-teal-700 px-4 py-1 text-white font-medium rounded-full hover:bg-white hover:text-emerald-500 border-[2px] border-teal-500">
+              Add Campground
+            </button>
+          </Link>
+        ) : null}
+      </div>
 
-      {profile.data.role == "admin" ? (
-        <Link href="/campground/manage/add">
-          <button className="absolute top-[17%] right-[12%] bg-teal-700 px-4 py-1 text-white font-medium rounded-full hover:bg-white hover:text-emerald-500 border-[2px] border-emerald-500">
-            Add Campground
-          </button>
-        </Link>
-      ) : null}
-
-      <Suspense
-        fallback={
-          <div className="flex justify-center items-center h-40">
-            <CircularProgress color="primary" />
-          </div>
-        }
-      >
-        <CampgroundCatalog campgroundJson={campgrounds} />
-      </Suspense>
     </main>
   );
 }
